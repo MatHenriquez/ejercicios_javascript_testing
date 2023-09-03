275abe433cfb4bf48d08b039a1dde019c024907e
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-03 11:16:09 -0300

    Test 1 done: added error handling

--- Tests/mayorEdad.test.js
+++ Tests/mayorEdad.test.js
@@ -1,1 +1,21 @@
+const mayorEdad = require("../Ejercicios/mayorEdad");
 
+describe("Funcionamiento de lógica", () => {
+  test('Debe retornar "Es menor de edad" al ingresar una edad menor a 18', () => {
+    expect(mayorEdad(17)).toBe("Usted es menor de edad");
+  });
+
+  test('Debe retornar "Es mayor de edad" al ingresar una edad mayor a 18', () => {
+    expect(mayorEdad(20)).toBe("Usted es mayor de edad");
+  });
+
+  test('Debe retornar "Es mayor de edad" al ingresar una edad igual a 18', () => {
+    expect(mayorEdad(18)).toBe("Usted es mayor de edad");
+  });
+});
+
+describe("Manejo de errores mínimo", () => {
+  test("Debe arrojar un error al ingresar un dato inválido", () => {
+    expect(mayorEdad("Hola")).toBe("El dato es inválido");
+  });
+});
