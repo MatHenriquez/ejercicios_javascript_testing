36219bcef8c50d774162dc925a53f6112d72fd61
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-03 11:15:42 -0300

    Exercise 1 done: added error handling

--- Ejercicios/mayorEdad.js
+++ Ejercicios/mayorEdad.js
@@ -1,21 +1,23 @@
 // Pide la edad y si es mayor de 18 años indica que ya puede conducir.
 // const readlineSync = require("readline-sync"); Para leer entrada por teclado
 
-function mayorEdad(testAge){
+function mayorEdad(testAge) {
+  // const inputAge = () => {  Para leer entrada por teclado
+  //   return readlineSync.question("Ingrese su edad: ");
+  // };
+  let legalAge = 18;
+  // let inputNumber = inputAge(); Para leer entrada por teclado
+  let message = "";
 
-// const inputAge = () => {  Para leer entrada por teclado
-//   return readlineSync.question("Ingrese su edad: ");
-// };
+  if (typeof testAge !== "number") {
+    message = "El dato es inválido";
+  } else {
+    testAge < legalAge
+      ? (message = "Usted es menor de edad")
+      : (message = "Usted es mayor de edad");
+  }
 
-let legalAge = 18;
-// let inputNumber = inputAge(); Para leer entrada por teclado
-let message = "";
-
-testAge < legalAge
-  ? (message = "Usted es menor de edad")
-  : (message = "Usted es mayor de edad");
-
-return message;
+  return message;
 }
 
 module.exports = mayorEdad;
