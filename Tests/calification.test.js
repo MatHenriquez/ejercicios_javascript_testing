ff30d67f29318637ec9d3a5fa1b4f3bb619f57e2
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-19 19:32:14 -0300

    third test now is in a new capsule

--- Tests/calification.test.js
+++ Tests/calification.test.js
@@ -13,13 +13,6 @@ describe("Debe retornar 'muy deficiente' si n es 0, 1 o 2:", () => {
     expect(calification(2)).toBe("Muy deficiente");
   });
 
-  test("Debe retornar 'Suficiente' si n es 5", () => {
-    expect(calification(2)).toBe("Muy deficiente");
-  });
-  test("Debe retornar 'Suficiente' si n es 6", () => {
-    expect(calification(0)).toBe("Muy deficiente");
-  });
-
   test("Debe retornar 'Suficiente' si n es 1", () => {
     expect(calification(1)).toBe("Muy deficiente");
   });
@@ -36,5 +29,14 @@ describe("Debe retornar 'Insuficiente", () => {
 
   test("Debe retornar 'Insuficiente' si n es 4", () => {
     expect(calification(1)).toBe("Muy deficiente");
+  });
+});
+
+describe("Debe retornar 'Suficiente' si n es 5 o 6", () => {
+  test("Debe retornar 'Suficiente' si n es 5", () => {
+    expect(calification(2)).toBe("Muy deficiente");
+  });
+  test("Debe retornar 'Suficiente' si n es 6", () => {
+    expect(calification(0)).toBe("Muy deficiente");
   });
 });
