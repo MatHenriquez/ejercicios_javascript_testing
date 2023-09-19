f0fb5be5340602a1283bb7630501188eec0abaf7
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-19 19:25:52 -0300

    first test: first case done

--- Tests/calification.test.js
+++ Tests/calification.test.js
@@ -1,1 +1,7 @@
-const calification = require('../Ejercicios/calification');
+const calification = require("../Ejercicios/calification");
+
+describe("Funcionamiento lógico:", () => {
+  test("Debe retornar 'muy deficiente' if n is 0, 1 or 2", () => {
+    expect(calification(0)).toBe("Muy deficiente");
+  });
+});
