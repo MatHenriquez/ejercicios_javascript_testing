274debea02c3ac09c7516a82277620ea7db77b3c
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-19 19:30:07 -0300

    second and thirst tests done

--- Tests/calification.test.js
+++ Tests/calification.test.js
@@ -1,11 +1,34 @@
 const calification = require("../Ejercicios/calification");
 
 describe("Debe retornar 'muy deficiente' si n es 0, 1 o 2:", () => {
-  test("Debe retornar 'muy deficiente' si n es 0", () => {
+  test("Debe retornar 'Muy deficiente' si n es 0", () => {
     expect(calification(0)).toBe("Muy deficiente");
   });
 
-  test("Debe retornar 'muy deficiente' si n es 1", () => {
+  test("Debe retornar 'Muy deficiente' si n es 1", () => {
+    expect(calification(1)).toBe("Muy deficiente");
+  });
+
+  test("Debe retornar 'Muy deficiente' si n es 2", () => {
+    expect(calification(2)).toBe("Muy deficiente");
+  });
+
+  test("Debe retornar 'Insuficiente' si n es 3", () => {
+    expect(calification(0)).toBe("Muy deficiente");
+  });
+
+  test("Debe retornar 'Insuficiente' si n es 4", () => {
+    expect(calification(1)).toBe("Muy deficiente");
+  });
+
+  test("Debe retornar 'Suficiente' si n es 5", () => {
+    expect(calification(2)).toBe("Muy deficiente");
+  });
+  test("Debe retornar 'Suficiente' si n es 6", () => {
+    expect(calification(0)).toBe("Muy deficiente");
+  });
+
+  test("Debe retornar 'Suficiente' si n es 1", () => {
     expect(calification(1)).toBe("Muy deficiente");
   });
 
