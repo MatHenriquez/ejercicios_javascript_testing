0c186281eefeb4e4220389a89414fae8fbb38030
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-22 17:37:58 -0300

    Exercise done

--- Ejercicios/fibonacci.js
+++ Ejercicios/fibonacci.js
@@ -1,3 +1,10 @@
-fibonacci = (n) => n === 0 || n === 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+// fibonacci = (n) => n === 0 || n === 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 
-console.log(fibonacci(4))
+function fibonacci(n) {
+
+    if(typeof n == 'String' || Math.floor(n) !== n || n < 0) return 'Error';
+
+    return n === 0 || n === 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+module.exports = fibonacci;
