6f2385591482a5019de862af4d57fffbeec0d200
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-25 22:29:40 -0300

    test:pending to complete

--- Tests/dniChar.test.ts
+++ Tests/dniChar.test.ts
@@ -4,28 +4,8 @@ const dniChar = require('../Ejercicios/dniChar');
 describe('dniChar', () => {
   it('Debe devolver la letra correcta para un número de DNI válido', () => {
     const dni = 123456789;
-    const expectedLetter = 'T';
+    const expectedLetter = 'B';
     const actualLetter = dniChar(dni);
     expect(actualLetter).toBe(expectedLetter);
-  });
-
-  xit('Debe devolver la letra correcta para un número de DNI de dos dígitos', () => {
-    const dni = 12;
-    const expectedLetter = 'Y';
-    const actualLetter = dniChar(dni);
-    expect(actualLetter).toBe(expectedLetter);
-  });
-
-  xit('Debe devolver la letra correcta para un número de DNI de un dígito', () => {
-    const dni = 1;
-    const expectedLetter = 'T';
-    const actualLetter = dniChar(dni);
-    expect(actualLetter).toBe(expectedLetter);
-  });
-
-  xit('Debe devolver una cadena vacía para un número de DNI no válido', () => {
-    const dni = '1234567890';
-    const actualLetter = dniChar(dni);
-    expect(actualLetter).toBe('');
   });
 });
