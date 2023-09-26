11566d42983a82924e5b37df41cbca31ad164a11
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-25 22:10:13 -0300

    Starting to code tests

---
+++ Tests/dniChar.test.js
@@ -0,0 +1,7 @@
+const dniChar = require("../Ejercicios/dniChar");
+
+describe("Manejo de errores", () => {
+    test("Debe estar definida", () => {
+        expect(dniChar).toBeDefined();
+    });
+    });
