9a94afe76c2c0ff6e41a74169f81f02934647773
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-25 22:25:35 -0300

    jest configuration for ts

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,20 @@
 // jest.config.js
 module.exports = {
-    // Archivos con pruebas (tests)
-    testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
-  
-    // Directorios donde buscar los módulos
-    moduleDirectories: ["node_modules", "src"],
-  
-    // Transformar archivos de JavaScript con Babel antes de ejecutar las pruebas
-    transform: {
-      "^.+\\.js$": "babel-jest",
-    },
-  };
-  
+  // Archivos con pruebas (tests)
+  testMatch: [
+    "**/__tests__/**/*.js",
+    "**/?(*.)+(spec|test).js",
+    "**/__tests__/**/*.ts",
+    "**/?(*.)+(spec|test).ts",
+  ],
+
+  // Directorios donde buscar los módulos
+  moduleDirectories: ["node_modules", "src"],
+
+  // Transformar archivos de JavaScript con Babel antes de ejecutar las pruebas
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
