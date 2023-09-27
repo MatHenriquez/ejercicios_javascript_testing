6bad3ca54856acfe71122199d0c9e0b14926cf8f
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-27 16:48:05 -0300

    tests done

--- Tests/iva.test.ts
+++ Tests/iva.test.ts
@@ -1,3 +1,19 @@
 import { describe, it, expect } from "@jest/globals";
 
 const applyIva = require("../Ejercicios/iva");
+
+describe("Funcionamiento:", () => {
+  it("Debe devolver el precio con IVA incluido", () => {
+    const price: number = 100;
+    const expectedPrice: number = 121;
+    const actualPrice: number = applyIva(price);
+    expect(actualPrice).toBe(expectedPrice);
+  });
+  it("Debe devolver el precio con IVA incluido", () => {
+    const price: number = 100;
+    const iva: number = 0.1;
+    const expectedPrice: number = 110;
+    const actualPrice: number = applyIva(price, iva);
+    expect(actualPrice).toBe(expectedPrice);
+  });
+});
